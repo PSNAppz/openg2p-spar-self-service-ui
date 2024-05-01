3d188abb8b7a6a44cc6f4b2c62a3906b9454d754
vineela-afk <ampilivineela@gmail.com>
2024-05-01 17:35:27 +0530

    made changes of linking and unlinking

---
+++ spar-self-service/.next/server/_rsc_messages_en_json.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "_rsc_messages_en_json";
+exports.ids = ["_rsc_messages_en_json"];
+exports.modules = {
+
+/***/ "(rsc)/./messages/en.json":
+/*!**************************!*\
+  !*** ./messages/en.json ***!
+  \**************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"NotFoundPage":{"title":"Page Not Found","description":"Oops! It seems like you\'ve stumbled upon a page that doesn\'t exist. Don\'t worry, our internationalization magic is still hard at work on other parts of the app. Feel free to navigate back to explore more!"},"Navigation":{"support":"Support","contact":"Contact"},"ProfileSet":{"profile":"My profile","log_out":"Log out"},"Update":{"update":"Update your account information","bank":"Bank","mobile_wallet":"Mobile Wallet","mpesa":"Mpesa","submit":"SUBMIT"},"Footer":{"copyright":"© 2024 Your App. All rights reserved."},"Status":{"thankyou":"Thank You","status_message":"Your account information is updated successfully"},"Delete":{"delete":"Removed","status_message":"Your account details removed successfully"},"Profile":{"name":"Name","ph_no":"Phone Number","email":"Email","dob":"Date of Birth","gender":"Gender","add":"Address"},"login":{"title":" Social Payments Account Registry","description_1":"With SPAR, Choose the account/wallet where you receive your","description_2":"social benefits in a simple,convenient,secure way","button_text":"PROCEED WITH NATIONAL ID"},"home":{"title":" Your account information","title_2":"Welcome to SPAR!","description":"Your account details are not mapped to the beneficiary program. Please choose where you want to recieve your cash benefits and update your account details","button_text":"UPDATE YOUR ACCOUNT DETAILS","button_text2":"Modify Account Details","button_text3":"Remove Account","bank":"Bank","branch":"Branch","account":"Account Number","mobile_provider":"Mobile Provider","mobile_number":"Mobile Number","email_provider":"Email Provider","email":"Email"}}');
+
+/***/ })
+
+};
+;
